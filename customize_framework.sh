daf52c03d1db27e5b7a85db6642ce09f64c5fa4e
Diorgenes <dyogenes_gd@hotmail.com>
2016-11-24 13:58:44 -0300

    Fix AbstractMethodError

--- customize_framework.sh
+++ customize_framework.sh
@@ -47,3 +47,8 @@ then
     rm -rf $2/smali/android/widget/Editor*
     cp -rf $1/smali/android/widget/Editor*.smali $2/smali/android/widget/
 fi
+
+if [ $2 = "$BUILD_OUT/services" ]
+then
+    applyPatch "overlay/services"
+fi
