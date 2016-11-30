db28e1629a7e65aebc8fbf8c943e75d652e706f3
Diorgenes <dyogenes_gd@hotmail.com>
2016-11-30 17:44:08 -0300

    Added: device_features

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -9,6 +9,9 @@ OTHER_DIR=$PWD/other
 cp -f other/file_contexts out/target_files/META/
 rm -rf out/target_files/SYSTEM/vendor/preinstall
 
+#Added device features
+cp -f other/serranolte.xml out/target_files/SYSTEM/etc/device_features
+
 #Added multi cust variants for miui
 rm -rf out/target_files/DATA/miui/cust
 cp -rf other/cust out/target_files/DATA/miui
